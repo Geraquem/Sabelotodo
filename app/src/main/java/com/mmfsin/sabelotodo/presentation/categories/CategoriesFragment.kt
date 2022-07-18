1dcec7343c06c7285868db3e8d3fca57ecafce70
gershon.araque <geraquem@gmail.com>
2022-07-18 11:35:29 +0200

    wip

--- app/src/main/java/com/mmfsin/sabelotodo/presentation/categories/CategoriesFragment.kt
+++ app/src/main/java/com/mmfsin/sabelotodo/presentation/categories/CategoriesFragment.kt
@@ -1,13 +1,16 @@
 package com.mmfsin.sabelotodo.presentation.categories
 
 import android.content.Context
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.mmfsin.sabelotodo.R
+import com.mmfsin.sabelotodo.R.string.*
 import com.mmfsin.sabelotodo.data.models.CategoryDTO
 import com.mmfsin.sabelotodo.databinding.FragmentCategoriesBinding
 import com.mmfsin.sabelotodo.databinding.ItemCategoryBinding
@@ -41,23 +44,50 @@ class CategoriesFragment(val listener: ICommunication) : Fragment(), CategoriesV
         with(binding) {
             for (category in categories) {
                 when (category.name) {
-                    getString(R.string.spanish_age) -> setData(category, cardOne)
-                    getString(R.string.global_age) -> setData(category, cardTwo)
-                    getString(R.string.cartoon_creations) -> setData(category, cardThree)
-                    getString(R.string.films_series) -> setData(category, cardFour)
-                    getString(R.string.videogames) -> setData(category, cardFive)
-                    getString(R.string.important_dates) -> setData(category, cardSix)
-                    getString(R.string.creation_objects) -> setData(category, cardSeven)
+                    getString(spanish_age) -> setData(category, cardOne, R.color.g_1, R.color.g_2)
+                    getString(global_age) -> setData(category, cardTwo, R.color.g_3, R.color.g_4)
+                    getString(cartoon_creations) -> setData(
+                        category,
+                        cardThree,
+                        R.color.g_5,
+                        R.color.g_6
+                    )
+                    getString(films_series) -> setData(category, cardFour, R.color.g_7, R.color.g_8)
+                    getString(videogames) -> setData(category, cardFive, R.color.g_9, R.color.g_10)
+                    getString(important_dates) -> setData(
+                        category,
+                        cardSix,
+                        R.color.g_11,
+                        R.color.g_12
+                    )
+                    getString(creation_objects) -> setData(
+                        category,
+                        cardSeven,
+                        R.color.g_5,
+                        R.color.g_6
+                    )
                 }
             }
         }
         /** Loading GONE */
     }
 
-    private fun setData(category: CategoryDTO, item: ItemCategoryBinding) {
+    private fun setData(
+        category: CategoryDTO,
+        item: ItemCategoryBinding,
+        color1: Int,
+        color2: Int
+    ) {
         item.title.text = category.title
         Glide.with(mContext).load(category.image).into(item.image)
         item.description.text = category.description
+
+        item.item.background = GradientDrawable().apply {
+            colors = intArrayOf(getColor(mContext, color1), getColor(mContext, color2))
+            gradientType = GradientDrawable.LINEAR_GRADIENT
+            orientation = GradientDrawable.Orientation.LEFT_RIGHT
+            cornerRadius = 48f
+        }
 
         item.item.setOnClickListener { listener.navigateToDashboard(category.name) }
     }
